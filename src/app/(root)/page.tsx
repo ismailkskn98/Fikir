bc269ee2cd8484021153e8338714efe3f730721a
İsmail Keskin <hamdismailkskn98@gmail.com>
2024-11-03 17:04:37 +0300

    tailwindCss config ayarları değiştirildi. Globals.css update

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,7 +1,15 @@
+import SearchForm from "@/components/SearchForm";
+
 export default function Home() {
   return (
     <>
-      <h1 className="heading text-red-600">Home</h1>
+      <section className="pink_container">
+        <h1 className="heading text-red-600">
+          Girişiminizi tanıtın, <br /> Girişimcilerle bağlantı kurun
+        </h1>
+        <p className="sub-heading !max-w-3xl">Fikirlerinizi gönderin, tekliflere oy verin ve sanal yarışmalarda fark edilin.</p>
+        <SearchForm />
+      </section>
     </>
   );
 }
